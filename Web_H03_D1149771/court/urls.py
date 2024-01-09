3cfc346db993fe6ccf0150e633f9d283631c6756
steven921025 <115069375+steven921025@users.noreply.github.com>
2024-01-09 23:39:12 +0800

    Delete Web_H03_D1149771 directory

--- Web_H03_D1149771/court/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('court/', views.court, name='court'),
-    path('court/detail/<int:id>',views.details,name = 'detail')
-]
